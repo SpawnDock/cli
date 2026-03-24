dc528f5abb2b8f3051b8f8b5ab557de2c31e535a
skulidropek <66840575+skulidropek@users.noreply.github.com>
2026-03-24 21:54:50 +0000

    fix(cli): prevent dist-deps-prune from removing required effect peer dependencies

--- packages/app/src/docker-git/main.ts
+++ packages/app/src/docker-git/main.ts
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
+import * as _1 from "@effect/cluster"
+import * as _2 from "@effect/experimental"
+import * as _3 from "@effect/printer"
+import * as _4 from "@effect/printer-ansi"
+import * as _5 from "@effect/rpc"
+import * as _6 from "@effect/sql"
+import * as _7 from "@effect/typeclass"
+import * as _8 from "@effect/workflow"
+import * as _9 from "@effect/cli"
+
+export const _dummyDeps = [_1, _2, _3, _4, _5, _6, _7, _8, _9]
+
 import { NodeContext, NodeRuntime } from "@effect/platform-node"
 import { Effect } from "effect"
 
